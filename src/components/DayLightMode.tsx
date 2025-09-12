90bf4385fb703d6e5f44a5db0252ffd9a69fb600
Yash Shinde <shindeyash5656@gmail.com>
2025-09-12 16:56:32 +0530

    trying DayLight Mode

---
+++ src/components/DayLightMode.tsx
@@ -0,0 +1,10 @@
+import BrightMode from "../inputButtonlogo/bright";
+import NightMode from "../inputButtonlogo/night";
+
+
+export function DayLightMode(){
+    return <div>
+        <NightMode />
+        <BrightMode />
+    </div>
+}
